98881eccc307728771a7f4df6f5ef8dd19b95f02
Muhammad Zachrie Kurniawan <zachrie2005@gmail.com>
2023-03-06 08:11:35 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import * as cocoModel from '@tensorflow-models/coco-ssd';
 const App = () => {
   const [model, setModel] = useState('');
   const [object, setObject] = useState([]);
+  const [isUserMode, setIsUserMode] = useState(true);
 
   const loadModel = async () => {
     try {
@@ -23,7 +24,11 @@ const App = () => {
     }
   };
 
-  const videoOption = { width: 720, height: 480, facingMode: 'user' };
+  const toggleUserMode = () => {
+    setIsUserMode((prev) => !prev);
+  }
+
+  const videoOption = { width: 720, height: 480, facingMode: isUserMode ? 'user' : 'environment' };
 
   useEffect(() => {
     tf.ready().then(() => {
@@ -35,9 +40,14 @@ const App = () => {
     <section className="min-h-screen bg-zinc-50 flex items-center justify-center">
       <div className="container mx-auto text-center">
         <h1 className="text-2xl text-zinc-900 font-bold mb-4">Hello World</h1>
-        <button className="px-4 py-2 bg-zinc-800 hover:bg-zinc-900 text-white rounded mb-4" onClick={predict}>
-          Predict
-        </button>
+        <div className="flex items-center justify-center gap-x-3">
+          <button className="px-4 py-2 bg-white hover:bg-zinc-900 border-2 border-zinc-900 text-zinc-900 rounded mb-4" onClick={toggleUserMode}>
+            Change Mode
+          </button>
+          <button className="px-4 py-2 bg-zinc-800 hover:bg-zinc-900 text-white rounded mb-4" onClick={predict}>
+            Predict
+          </button>
+        </div>
         <ul>
           {object &&
             object?.map((result, index) => (
