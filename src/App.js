7de5eeaeb512e63822ae8b5ecb476756b28c2618
Muhammad Zachrie Kurniawan <zachrie2005@gmail.com>
2023-03-06 07:19:12 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import Webcam from 'react-webcam';
+import * as tf from '@tensorflow/tfjs';
+import * as cocoModel from '@tensorflow-models/coco-ssd';
 
-function App() {
+const App = () => {
+  const [model, setModel] = useState('');
+  const [object, setObject] = useState([]);
+
+  const loadModel = async () => {
+    try {
+      const dataset = await cocoModel.load();
+      setModel(dataset);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const predict = async () => {
+    const detection = await model.detect(document.getElementById('video'));
+    if (detection.length > 0) {
+      setObject(detection);
+    }
+  };
+
+  const videoOption = { width: 720, height: 480, facingMode: 'environment' };
+
+  useEffect(() => {
+    tf.ready().then(() => {
+      loadModel();
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="min-h-screen bg-zinc-50 flex items-center justify-center">
+      <div className="container mx-auto text-center">
+        <h1 className="text-2xl text-zinc-900 font-bold mb-4">Hello World</h1>
+        <button className="px-4 py-2 bg-zinc-800 hover:bg-zinc-900 text-white rounded mb-4" onClick={predict}>
+          Predict
+        </button>
+        <ul>
+          {object &&
+            object?.map((result, index) => (
+              <li key={index}>
+                {result.class.toString()} - {result.score.toString()}
+              </li>
+            ))}
+        </ul>
+        <Webcam audio={false} videoConstraints={videoOption} className="mt-6 mx-auto" id="video" />
+      </div>
+    </section>
   );
-}
+};
 
 export default App;
